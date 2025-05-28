d8cef2de408c6ba498e77e616867e0a37370be1a
natnuo <natnu212@gmail.com>
2025-05-27 21:01:32 -0400

    learning three.js, moving flag setup done

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,15 +1,127 @@
-import React, { useRef } from "react";
 import styles from "../css/index.module.css";
-import { io } from "socket.io-client";
+import { Canvas, useFrame } from '@react-three/fiber'
+import Memory from "./Grave";
+import { useRef, useState } from "react";
+import { CameraControls } from "@react-three/drei";
+
+const RED = "#B31942", BLUE = "#0A3161", WHITE = "#FFFFFF";
 
 const App = () => {
-  const __PRODUCTION__ = useRef(window.location.hostname !== "localhost");
-
-  const socket = io(`ws://${window.location.hostname}:`)
+  const [paused, setPaused] = useState(true);
 
   return (
-    <div className={`${styles["w-full"]} ${styles["h-full"]}`}>
-      HI!
+    <div className={`${styles["w-svw"]} ${styles["h-svh"]}`}>
+      <Canvas flat className={`${styles["w-full"]} ${styles["h-full"]} ${styles["bg-black"]}`}>
+        {/* CAMERA */}
+        <CameraControls
+          onChange={() => setPaused(false) }
+          minPolarAngle={0}
+          maxPolarAngle={Math.PI / 1.6}
+        />
+
+        {/* LIGHTING */}
+        <ambientLight intensity={Math.PI / 2} />
+        <spotLight
+          position={[ 0, 0, 100 ]}
+          angle={0.15}
+          rotation={[ Math.PI / 2, 0, 0 ]}
+          penumbra={1}
+          decay={0}
+          intensity={Math.PI / 2}
+        />
+
+        {/* MESHES */}
+        {/* cemetery */}
+        <group position={[ 0, 0, -10 ]}>
+          <Memory x={-5} y={3} color={BLUE} paused={paused}></Memory>
+          <Memory x={-5} y={2} color={BLUE} paused={paused}></Memory>
+          <Memory x={-5} y={1} color={BLUE} paused={paused}></Memory>
+          <Memory x={-5} y={0} color={WHITE} paused={paused}></Memory>
+          <Memory x={-5} y={-1} color={RED} paused={paused}></Memory>
+          <Memory x={-5} y={-2} color={WHITE} paused={paused}></Memory>
+          <Memory x={-5} y={-3} color={RED} paused={paused}></Memory>
+          
+          <Memory x={-4} y={3} color={BLUE} paused={paused}></Memory>
+          <Memory x={-4} y={2} color={BLUE} paused={paused}></Memory>
+          <Memory x={-4} y={1} color={BLUE} paused={paused}></Memory>
+          <Memory x={-4} y={0} color={WHITE} paused={paused}></Memory>
+          <Memory x={-4} y={-1} color={RED} paused={paused}></Memory>
+          <Memory x={-4} y={-2} color={WHITE} paused={paused}></Memory>
+          <Memory x={-4} y={-3} color={RED} paused={paused}></Memory>
+
+          <Memory x={-3} y={3} color={BLUE} paused={paused}></Memory>
+          <Memory x={-3} y={2} color={BLUE} paused={paused}></Memory>
+          <Memory x={-3} y={1} color={BLUE} paused={paused}></Memory>
+          <Memory x={-3} y={0} color={WHITE} paused={paused}></Memory>
+          <Memory x={-3} y={-1} color={RED} paused={paused}></Memory>
+          <Memory x={-3} y={-2} color={WHITE} paused={paused}></Memory>
+          <Memory x={-3} y={-3} color={RED} paused={paused}></Memory>
+            
+          <Memory x={-2} y={3} color={BLUE} paused={paused}></Memory>
+          <Memory x={-2} y={2} color={BLUE} paused={paused}></Memory>
+          <Memory x={-2} y={1} color={BLUE} paused={paused}></Memory>
+          <Memory x={-2} y={0} color={WHITE} paused={paused}></Memory>
+          <Memory x={-2} y={-1} color={RED} paused={paused}></Memory>
+          <Memory x={-2} y={-2} color={WHITE} paused={paused}></Memory>
+          <Memory x={-2} y={-3} color={RED} paused={paused}></Memory>
+          
+          <Memory x={-1} y={3} color={RED} paused={paused}></Memory>
+          <Memory x={-1} y={2} color={WHITE} paused={paused}></Memory>
+          <Memory x={-1} y={1} color={RED} paused={paused}></Memory>
+          <Memory x={-1} y={0} color={WHITE} paused={paused}></Memory>
+          <Memory x={-1} y={-1} color={RED} paused={paused}></Memory>
+          <Memory x={-1} y={-2} color={WHITE} paused={paused}></Memory>
+          <Memory x={-1} y={-3} color={RED} paused={paused}></Memory>
+            
+          <Memory x={0} y={3} color={RED} paused={paused}></Memory>
+          <Memory x={0} y={2} color={WHITE} paused={paused}></Memory>
+          <Memory x={0} y={1} color={RED} paused={paused}></Memory>
+          <Memory x={0} y={0} color={WHITE} paused={paused}></Memory>
+          <Memory x={0} y={-1} color={RED} paused={paused}></Memory>
+          <Memory x={0} y={-2} color={WHITE} paused={paused}></Memory>
+          <Memory x={0} y={-3} color={RED} paused={paused}></Memory>
+          
+          <Memory x={1} y={3} color={RED} paused={paused}></Memory>
+          <Memory x={1} y={2} color={WHITE} paused={paused}></Memory>
+          <Memory x={1} y={1} color={RED} paused={paused}></Memory>
+          <Memory x={1} y={0} color={WHITE} paused={paused}></Memory>
+          <Memory x={1} y={-1} color={RED} paused={paused}></Memory>
+          <Memory x={1} y={-2} color={WHITE} paused={paused}></Memory>
+          <Memory x={1} y={-3} color={RED} paused={paused}></Memory>
+          
+          <Memory x={2} y={3} color={RED} paused={paused}></Memory>
+          <Memory x={2} y={2} color={WHITE} paused={paused}></Memory>
+          <Memory x={2} y={1} color={RED} paused={paused}></Memory>
+          <Memory x={2} y={0} color={WHITE} paused={paused}></Memory>
+          <Memory x={2} y={-1} color={RED} paused={paused}></Memory>
+          <Memory x={2} y={-2} color={WHITE} paused={paused}></Memory>
+          <Memory x={2} y={-3} color={RED} paused={paused}></Memory>
+          
+          <Memory x={3} y={3} color={RED} paused={paused}></Memory>
+          <Memory x={3} y={2} color={WHITE} paused={paused}></Memory>
+          <Memory x={3} y={1} color={RED} paused={paused}></Memory>
+          <Memory x={3} y={0} color={WHITE} paused={paused}></Memory>
+          <Memory x={3} y={-1} color={RED} paused={paused}></Memory>
+          <Memory x={3} y={-2} color={WHITE} paused={paused}></Memory>
+          <Memory x={3} y={-3} color={RED} paused={paused}></Memory>
+          
+          <Memory x={4} y={3} color={RED} paused={paused}></Memory>
+          <Memory x={4} y={2} color={WHITE} paused={paused}></Memory>
+          <Memory x={4} y={1} color={RED} paused={paused}></Memory>
+          <Memory x={4} y={0} color={WHITE} paused={paused}></Memory>
+          <Memory x={4} y={-1} color={RED} paused={paused}></Memory>
+          <Memory x={4} y={-2} color={WHITE} paused={paused}></Memory>
+          <Memory x={4} y={-3} color={RED} paused={paused}></Memory>
+          
+          <Memory x={5} y={3} color={RED} paused={paused}></Memory>
+          <Memory x={5} y={2} color={WHITE} paused={paused}></Memory>
+          <Memory x={5} y={1} color={RED} paused={paused}></Memory>
+          <Memory x={5} y={0} color={WHITE} paused={paused}></Memory>
+          <Memory x={5} y={-1} color={RED} paused={paused}></Memory>
+          <Memory x={5} y={-2} color={WHITE} paused={paused}></Memory>
+          <Memory x={5} y={-3} color={RED} paused={paused}></Memory>
+        </group>
+      </Canvas>
     </div>
   );
 };
---
+++ client/src/components/Grave.tsx
@@ -0,0 +1,96 @@
+import { useFrame } from "@react-three/fiber";
+import { ComponentProps, useEffect, useRef, useState } from "react";
+import { Color, Mesh, Object3D } from "three";
+
+// will be further modified by dt
+const MAX_ACCEL = 0.01;
+const MAX_V = 0.05;
+
+const MAX_TGPZ = 20;  // min will be additive opposite
+
+const MIN_TGPZ_CHANGE = 2;
+
+type MemoryProps = {
+  x: number;
+  y: number;
+  color: string | Color;
+  paused: boolean;
+};
+
+// HAVE MEMORIES THAT CAN BE INTERACTED WITH, GLOW
+
+const pController = (v: number, tgp: number, p: number, dt: number, Kp: number) => {
+  const mx_a_mag = MAX_ACCEL * dt;  // max acceleration magnitude
+
+  const et = tgp - p;
+  const tgv = Kp * et;  // p controller
+  const az = Math.max(Math.min(tgv - v, mx_a_mag), -mx_a_mag);
+  
+  const v_ = Math.max(Math.min(v + az, MAX_V), -MAX_V);
+
+  // simple calc so high dt will cause inaccuracy, but high accuracy unnecessary
+  // and high dt should is filtered before our main use of this function
+  const p_ = p + v_;
+
+  return { p: p_, v: v_, et };
+};
+
+export default function Memory({ x, y, color, paused }: MemoryProps) {
+  const meshRef = useRef<Mesh>(null);
+
+  const [active, setActive] = useState(false);
+
+  // useEffect(() => {
+  //   if (!meshRef.current) return;
+
+  //   meshRef.current.position.x = x;
+  //   meshRef.current.position.y = y;
+  // }, []);
+
+// todo: american flag-bound movement
+
+  let vz = 0, tgpz = 0, pz = 0;
+  let vx = 0, tgpx = 2*x, px = x;
+  let vy = 0, tgpy = 2*y, py = y;
+  useFrame((state, dt) => {
+    if (dt > 0.5) dt = 0;
+
+    // theoretically should track cumulative dt here but theoretically meshRef.current will exist anyway
+    // so _theoretically_ is fine, lol/
+    if (!meshRef.current) return;
+
+    meshRef.current.position.x = px;
+    meshRef.current.position.y = py;
+    meshRef.current.position.z = pz;
+
+    if (paused) return;
+
+    const { v: vx_, p: px_ } = pController(vx, tgpx, px, dt, 0.001); vx = vx_; px = px_;
+    const { v: vy_, p: py_ } = pController(vy, tgpy, py, dt, 0.001); vy = vy_; py = py_;
+    
+    const { v: vz_, p: pz_, et: etz } = pController(vz, tgpz, pz, dt, 0.003);
+    pz = pz_;
+    vz = vz_;
+      console.log(tgpz, meshRef.current.position.z);
+
+    // console.log(tgpz, pz);
+
+    if (Math.abs(etz) < 0.5) {
+      let n_tgpz;
+      do {
+        n_tgpz = Math.random() * 2 * MAX_TGPZ - MAX_TGPZ;
+      } while (Math.abs(n_tgpz - tgpz) < MIN_TGPZ_CHANGE);
+
+      console.log(tgpz);
+
+      tgpz = n_tgpz;
+    }
+  });
+
+  return (<>
+    <mesh ref={meshRef} onClick={() => setActive(!active)}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={color} /> 
+    </mesh>
+  </>);
+}
